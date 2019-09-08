462576273b387fe603d5ac7eed31bad387035bcb
mattdsteele <orphum@gmail.com>
2019-09-07 22:10:11 -0500

    Actually invoke Particle API

--- particle.sh
+++ particle.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -l
 
-echo "Hello world!"
-
 echo "You want me to run '$FUNCTION_NAME' on device $DEVICE_ID, eh?"
+
+curl https://api.particle.io/v1/devices/${DEVICE_ID}/${FUNCTION_NAME} \
+#       -d arg="ZEN PLEASE" \
+       -d access_token=${ACCESS_TOKEN}
